28414a9bf8f9a09caf1d1b6c9126afdadc2ee783
maxholndrwbrg <max.hollanderwiborg@skola.taby.se>
2024-12-05 21:13:07 +0100

    dragon-slayer-klar

--- Ny mapp/dragon-slayer.py
+++ Ny mapp/dragon-slayer.py
@@ -1,179 +1,176 @@
-# def meny():
-#     print("""
-#     Välkommen!
-          
-#     Välj ett alternativ:
-#     [1] Spela
-#     [2] Se ditt inventory
-#     [3] Se dina stats
-#     [4] Avsluta
-#     """)
+import random as slump
+import sys
+import time
 
-# def spel_meny():
-#     print("""
-#     Du har fått ett vapen - 
-#     Spelmeny:
-#     [1] Attackera en fiende
-#     [2] Utforska området
-#     [3] Gå tillbaka till huvudmenyn
-#     """)
-
-# def get_user_input():
-#     try:
-#         tal = int(input("Gör ett val: "))
-#     except ValueError:
-#         print("Det var inte ett heltal, välj ett annat alternativ.")
-#         return 0
-#     except Exception as ex:
-#         print(f"Något gick fel: {ex}")
-#         return 0
-#     return tal
-
-# dmg = 0
-# hp = 100
-
-# while True:
-#     meny()
-#     val = get_user_input()
-    
-#     if val == 0:
-#         continue
-#     if val == 1:
-#         while True:
-#             spel_meny()
-#             spel_val = get_user_input()
-#             if spel_val == 0:
-#                 continue
-#             if spel_val == 1:
-#                 print("dungeon 1")
-#             elif spel_val == 2:
-#                 print("dungeon 2")
-#             elif spel_val == 3:
-#                 print("Dungeon 3")
-#             else:
-#                 print("Ogiltigt val, försök igen.")
-
-#     elif val == 2:
-#         print("Ditt inventory är tomt än så länge.")
-#     elif val == 3:
-#         print(f""" 
-              
-              
-# Dina stats:
-#     - Damage: {dmg}
-#     - HP: {hp}
-# """)
-#     elif val == 4:
-#         print("Spelet avslutas.")
-#         break
-#     else:
-#         print("Ogiltigt val, försök igen.")
-
-
-
+# Klass för spelare
 class Spelare:
-    def __init__(self, namn):
+    def __init__(self, hp, styrka, tur, nivå, namn):
+        self.hp = hp
+        self.styrka = styrka
+        self.tur = tur
+        self.nivå = nivå
+        self.erfarenhet = 0  # Ny variabel för erfarenhet
+        self.förråd = []  # Förråd för föremål
         self.namn = namn
-        self.damage = 0
-        self.hp = 100
-        self.inventory = []
 
-    def visa_stats(self):
-        print(f"""
-Dina stats:
-    - Damage: {self.damage}
-    - HP: {self.hp}
-        """)
-
-    def visa_inventory(self):
-        if self.inventory:
-            print("Ditt inventory innehåller:")
-            for item in self.inventory:
-                print(f"- {item}")
-        else:
-            print("Ditt inventory är tomt än så länge.")
+    def visa_statistik(self):
+        print(f"\n{self.namn}s Statistik:")
+        print(f"Hälsa: {self.hp}")
+        print(f"Styrka: {self.styrka}")
+        print(f"Tur: {self.tur}")
+        print(f"Nivå: {self.nivå}")
+        print(f"Erfarenhet: {self.erfarenhet}\n")
 
     def lägg_till_föremål(self, föremål):
-        self.inventory.append(föremål)
-        print(f"{föremål} har lagts till i ditt inventory.")
+        if len(self.förråd) < 15:  # Max 15 föremål
+            self.förråd.append(föremål)
+            print(f"Du lade till {föremål.namn} i ditt förråd.")
+            self.styrka += föremål.styrka_bonus
+            self.hp += föremål.hälsa_bonus
+            self.tur += föremål.tur_bonus
+        else:
+            print("Ditt förråd är fullt! Du kan inte ta fler föremål.")
 
+    def visa_förråd(self):
+        if self.förråd:
+            print("\nDitt förråd innehåller:")
+            for i, föremål in enumerate(self.förråd, start=1):
+                print(f"[{i}] {föremål}")
+        else:
+            print("\nDitt förråd är tomt.")
 
-class Spel:
-    def __init__(self):
-        self.spelare = None
+    def öka_erfarenhet(self, mängd):
+        self.erfarenhet += mängd
+        print(f"Du fick {mängd} erfarenhetspoäng!")
+        if self.erfarenhet >= 50:  # Mängd erfarenhet för att gå upp i nivå
+            self.erfarenhet = 0
+            self.nivå += 1
+            print(f"Grattis! Du har gått upp till nivå {self.nivå}!")
+            if self.nivå == 3:
+                print("Du har nått nivå 3! Spelet är slut!")
+                sys.exit()  # Avsluta spelet när spelaren når nivå 3
 
-    def visa_meny(self):
-        print("""
-    Välj ett alternativ:
-    [1] Spela
-    [2] Se ditt inventory
-    [3] Se dina stats
-    [4] Avsluta
-        """)
+# Klass för föremål
+class Föremål:
+    def __init__(self, styrka_bonus, hälsa_bonus, tur_bonus, namn):
+        self.styrka_bonus = styrka_bonus
+        self.hälsa_bonus = hälsa_bonus
+        self.tur_bonus = tur_bonus
+        self.namn = namn
 
-    def spel_meny(self):
-        print("""
-    Spelmeny:
-    [1] Attackera en fiende
-    [2] Utforska området
-    [3] Gå tillbaka till huvudmenyn
-        """)
+    def __str__(self):
+        return f"{self.namn} (Styrka: {self.styrka_bonus}, Hälsa: {self.hälsa_bonus}, Tur: {self.tur_bonus})"
 
+# Funktioner för att skapa föremål
+def skapa_svärd():
+    return Föremål(slump.randint(1, 10), 0, 0, "Svärd")
 
-    import random
+def skapa_dryck():
+    return Föremål(0, slump.randint(10, 30), 0, "Hälsodryck")
 
-    class vapen:
-        def __init__(self, vapen_dmg, vapen_namn):
-            self.vapen_dmg = vapen_dmg
-            self.vapen_namn= vapen_namn
-            self.inventory 
+def skapa_turarmband():
+    return Föremål(0, 0, slump.randint(1, 5), "Turarmband")
 
+# Text med fördröjning
+def skriv_med_fördröjning(text, fördröjning=0.02):
+    for tecken in text:
+        sys.stdout.write(tecken)
+        sys.stdout.flush()
+        time.sleep(fördröjning)
+    print()
 
-  def starta_spel(self, self_spelare):
-        namn = input("Vad heter din karaktär? ")
-        self_spelare = Spelare(namn)
-        print(f"Välkommen till spelet, {self_spelare.namn}!") 
+# Funktion för att skapa slumpmässiga rum
+def slumpmässigt_rum():
+    rum_typ = slump.choice(["drake", "fälla", "kista"])
+    if rum_typ == "drake":
+        möt_drake()
+    elif rum_typ == "fälla":
+        gå_in_i_fälla()
+    elif rum_typ == "kista":
+        öppna_kista()
 
-        while True:
-            self.visa_meny()
-            val = self.get_user_input()
-            
-            if val == 1:
-                self.spel_loop()
-            elif val == 2:
-                self.spelare.visa_inventory()
-            elif val == 3:
-                self.spelare.visa_stats()
-            elif val == 4:
-                print("Spelet avslutas.")
-                break
+# Rumsfunktioner
+def möt_drake():
+    skriv_med_fördröjning("En fruktansvärd drake hoppar fram!")
+    drake_hp = slump.randint(10, 30)
+    drake_styrka = slump.randint(3, 10)
+    print(f"Drakens hälsa: {drake_hp}, Styrka: {drake_styrka}")
+
+    while drake_hp > 0 and spelare.hp > 0:
+        print("\nVad vill du göra?")
+        print("[1] Attackera")
+        print("[2] Försök fly")
+        val = input("Ange ditt val: ")
+
+        if val == "1":
+            skada = slump.randint(1, spelare.styrka)
+            drake_hp -= skada
+            if drake_hp < 0:
+                drake_hp = 0  
+            print(f"Du slog draken för {skada} skada! Drakens hälsa: {drake_hp}")
+            if drake_hp > 0:
+                skada_tillbaka = slump.randint(1, drake_styrka)
+                spelare.hp -= skada_tillbaka
+                print(f"Draken attackerade dig för {skada_tillbaka} skada! Din hälsa: {spelare.hp}")
+        elif val == "2":
+            if slump.randint(1, 10) + spelare.tur > 8:
+                print("Du lyckades fly!")
+                return
             else:
-                print("Ogiltigt val, försök igen.")
+                print("Du misslyckades med att fly!")
+                skada_tillbaka = slump.randint(1, drake_styrka)
+                spelare.hp -= skada_tillbaka
+                print(f"Draken attackerade dig för {skada_tillbaka} skada! Din hälsa: {spelare.hp}")
 
-    def spel_loop(self):
-        while True:
-            self.spel_meny()
-            spel_val = self.get_user_input()
-            if spel_val == 1:
-                print("Du attackerar en fiende!")
-                self.spelare.damage += 5  
-            elif spel_val == 2:
-                print("Du utforskar området och hittar ett svärd!")
-                self.spelare.lägg_till_föremål("Svärd")
-            elif spel_val == 3:
-                print("Du återvänder till huvudmenyn.")
-                break
-            else:
-                print("Ogiltigt val, försök igen.")
+    if drake_hp == 0:
+        print("Du besegrade draken!")
+        spelare.öka_erfarenhet(10)  # Spelaren får erfarenhet för att besegra draken
+    if spelare.hp <= 0:
+        print("Du blev besegrad av draken... Spelet är över!")
+        sys.exit()
 
-    def get_user_input(self):
-        try:
-            tal = int(input("Gör ett val: "))
-            return tal
-        except ValueError:
-            print("Det var inte ett heltal, välj ett annat alternativ.")
-        return 0
+def gå_in_i_fälla():
+    print("Du aktiverade en fälla!")
+    skada = slump.randint(5, 15)
+    spelare.hp -= skada
+    print(f"Du tog {skada} skada. Din hälsa är nu {spelare.hp}.")
+    if spelare.hp <= 0:
+        print("Du dog av skadorna... Spelet är över!")
+        sys.exit()
 
+def öppna_kista():
+    print("Du hittar en kista!")
+    föremål_typ = slump.choice([skapa_svärd, skapa_dryck, skapa_turarmband])
+    föremål = föremål_typ()
+    print(f"Du hittade {föremål}!")
+    spelare.lägg_till_föremål(föremål)
+    spelare.öka_erfarenhet(5)  # Spelaren får erfarenhet för att öppna kistan
 
-spel = Spel()
-spel.starta_spel()
+# Startmeny
+def start_meny():
+    skriv_med_fördröjning("\nVälkommen till Dragonslayer!\n")
+    namn = input("Vad heter din karaktär? ")
+    global spelare
+    spelare = Spelare(100, 10, 5, 1, namn)
+    while True:
+        print("\nVad vill du göra?")
+        print("[1] Gå in i en läskig grotta")
+        print("[2] Visa din statistik")
+        print("[3] Visa ditt förråd")
+        print("[4] Avsluta spelet")
+        val = input("Vad väljer du?: ")
+
+        if val == "1":
+            slumpmässigt_rum()
+        elif val == "2":
+            spelare.visa_statistik()
+        elif val == "3":
+            spelare.visa_förråd()
+        elif val == "4":
+            print("Tack för att du spelade! Hejdå!")
+            break
+        else:
+            print("Ogiltigt val. Försök igen.")
+
+# Starta spelet
+start_meny()
